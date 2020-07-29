dae865353e6e4431403dc49539083735f25d0bb4
snakista <sitanshyadav@gmail.com>
2020-07-29 20:15:56 +0530

    Added files are changed

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 
 app.use(helmet());
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Sitanshu:31122001@cluster0.89lew.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Sitanshu:31122001@cluster0.89lew.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB=process.env.MONGODB_URI||dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
